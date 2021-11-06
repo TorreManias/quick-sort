7505ccbe77c537df8b485e58654e79c5989da708
TorreManias <correosinnombre02@gmail.com>
2021-11-06 17:18:10 -0600

    QuickSort

---
+++ src/QuickSort.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : QuickSort.cpp
+// Author      : Marlie Ramirez, Marcos Lanuza y Luis Orozco
+// Description : Algoritmo de ordenamiento - QuickSort
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+void qSort(int[], int l, int r) {
+	// Función recursiva del QuickSort
+	if (l >= r)
+		return;
+	//TODO: implementar la recursividad de la función
+}
+
+void particionar() {
+	// TODO: Esta función debería regresar el índice del elemento pivot en su lugar correcto
+}
+
+int main() {
+	int desordenado = {5, 10, 0, 9, 4, 1, 20, 13, 7, 6, 89, 123, 45, 23};
+	return 0;
+}
