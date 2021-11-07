4c7d3c9d04cead18eff47dedd154d3dcc7b25567
TorreManias <correosinnombre02@gmail.com>
2021-11-07 17:51:57 -0600

    Algoritmo implementado

--- src/QuickSort.cpp
+++ src/QuickSort.cpp
@@ -12,45 +12,46 @@
 
 using namespace std;
 
-int particionar(int arr[], int l, int r) {
-	// TODO: Esta funciï¿½n deberï¿½a regresar el ï¿½ndice del elemento pivot en su lugar correcto
-	int pivote = 0;
-
-	// Índices para recorrer el arreglo
-	int i = l - 1;
-	int j = l;
-
-	for (j; j < r; j++) {
-		if (arr[j] <= arr[r]) {
-			// Intercambiar los números
-			i++;
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}
-	}
-
-	return pivote;
-}
-
-void qSort(int *arr, int l, int r) {
-	// Funciï¿½n recursiva del QuickSort
-	if (l >= r)
-		return;
-
-	int indexPivote = particionar(arr, l, r);
-
-	// Ordenar a la izquierda del pivote
-	qSort(arr, 0, indexPivote - 1);
-	// Ordenar a la derecha del pivote
-	qSort(arr, indexPivote + 1, r);
-}
-
 void imprimirArr(int arr[], int TAM){
 	for (int i = 0; i < TAM; i++) {
 		cout << arr[i] << ", ";
 	}
 	cout << endl;
+}
+
+void intercambiar(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int particionar(int arr[], int l, int r) {
+	// TODO: Esta funciï¿½n deberï¿½a regresar el ï¿½ndice del elemento pivot en su lugar correcto
+	int pivote = arr[r];
+
+	// Índices para recorrer el arreglo
+	int i = l - 1;
+	for (int j = l; j <= (r - 1); j++) {
+		if (arr[j] < pivote) {
+			// Intercambiar los números
+			i++;
+			intercambiar(&arr[i], &arr[j]);
+		}
+	}
+	intercambiar(&arr[i + 1], &arr[r]);
+	return i + 1;
+}
+
+void qSort(int arr[], int l, int r) {
+	// Funciï¿½n recursiva del QuickSort
+	if (l < r) {
+		int indexPivote = particionar(arr, l, r);
+
+		// Ordenar a la izquierda del pivote
+		qSort(arr, l, indexPivote - 1);
+		// Ordenar a la derecha del pivote
+		qSort(arr, indexPivote + 1, r);
+	}
 }
 
 void generarNumerosAleatorios(int *arr, int TAM) {
@@ -67,7 +68,7 @@ int main(int argc, char *argv[]) {
 	switch(argc){
 		case 2: {
 
-			if (isdigit(atoi(argv[1]))) {
+			//if (isdigit(atoi(argv[1]))) {
 				int TAM = atoi(argv[1]);
 				cout << "Tamaño del arreglo: " << TAM << endl;
 
@@ -76,9 +77,13 @@ int main(int argc, char *argv[]) {
 
 				cout << "Arreglo desordenado: ";
 				imprimirArr(numeros, TAM);
-			} else {
+
+				cout << "Arreglo ordenado" << endl;
+				qSort(numeros, 0, TAM - 1);
+				imprimirArr(numeros, TAM);
+			/*} else {
 				cout << "Argumento inválido" << endl;
-			}
+			} */
 
 		}
 
