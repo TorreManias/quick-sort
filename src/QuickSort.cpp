d8d994992d3155106e236588ca540e441586ae7d
TorreManias <correosinnombre02@gmail.com>
2021-11-07 18:14:56 -0600

    Cálculo del tiempo del algoritmo

--- src/QuickSort.cpp
+++ src/QuickSort.cpp
@@ -9,6 +9,7 @@
 #include <time.h>
 #include <locale.h>
 #include <ctype.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -29,11 +30,11 @@ int particionar(int arr[], int l, int r) {
 	// TODO: Esta funciï¿½n deberï¿½a regresar el ï¿½ndice del elemento pivot en su lugar correcto
 	int pivote = arr[r];
 
-	// Índices para recorrer el arreglo
+	// ï¿½ndices para recorrer el arreglo
 	int i = l - 1;
 	for (int j = l; j <= (r - 1); j++) {
 		if (arr[j] < pivote) {
-			// Intercambiar los números
+			// Intercambiar los nï¿½meros
 			i++;
 			intercambiar(&arr[i], &arr[j]);
 		}
@@ -63,6 +64,8 @@ void generarNumerosAleatorios(int *arr, int TAM) {
 
 int main(int argc, char *argv[]) {
 
+	clock_t inicio, fin, total;
+
 	setlocale(LC_ALL, "spanish");
 
 	switch(argc){
@@ -70,19 +73,23 @@ int main(int argc, char *argv[]) {
 
 			//if (isdigit(atoi(argv[1]))) {
 				int TAM = atoi(argv[1]);
-				cout << "Tamaño del arreglo: " << TAM << endl;
+				cout << "Tamaï¿½o del arreglo: " << TAM << endl;
 
+				
 				int numeros[TAM] = {0};
 				generarNumerosAleatorios(numeros, TAM);
 
-				cout << "Arreglo desordenado: ";
 				imprimirArr(numeros, TAM);
 
-				cout << "Arreglo ordenado" << endl;
+				inicio = clock();
 				qSort(numeros, 0, TAM - 1);
+				fin = clock();
 				imprimirArr(numeros, TAM);
+				total = (double)((fin - inicio) / CLOCKS_PER_SEC);
+				printf("\nTiempo total de ordenamiento de %d nï¿½meros: %f\n", TAM, total);
+
 			/*} else {
-				cout << "Argumento inválido" << endl;
+				cout << "Argumento invï¿½lido" << endl;
 			} */
 
 		}
@@ -90,10 +97,11 @@ int main(int argc, char *argv[]) {
 			break;
 
 		default:
-			cout << endl << "Número inválido de argumentos. Intente de nuevo con el siguiente formato:" << endl;
+			cout << endl << "Nï¿½mero invï¿½lido de argumentos. Intente de nuevo con el siguiente formato:" << endl;
 			cout << "\t" << "a.exe [cantidad_aleatorios_generar]" << endl;
 			break;
 	}
+
 	system("pause");
 	return 0;
 }
